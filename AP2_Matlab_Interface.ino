57bbbeaf2e430b9e7ed2c41c89ab7abc71e3cab6
Justin-Womack <44512522+Justin-Womack@users.noreply.github.com>
2018-11-07 00:50:41 -0800

    Add files via upload

---
+++ AP2_Matlab_Interface.ino
@@ -0,0 +1,45 @@
+#define POT_PIN A0
+
+String matlab_input = "";
+bool  matlab_input_done = false;
+String data = "";
+
+long input = 0;
+long output = 0; 
+
+void setup() {
+	Serial.begin(2000000);						//set serial baudrate to 9600
+  	matlab_input.reserve(200);				//reserve space for the matlab input string
+}
+
+void loop() {
+	//check if matlab wants data
+	if(matlab_input_done == true){ 
+		
+		if(matlab_input == "send_data\n")
+			Serial.println(data);
+		
+		matlab_input = "";					//reset the input string
+		
+		matlab_input_done = false;			//reset the input flag
+	}
+
+	long input = analogRead(POT_PIN);
+  
+  	long output = input * 255/1023;
+
+  	data = String(output);
+  	//data += (char) '\n';
+}
+
+
+void serialEvent(){
+	while(Serial.available()){
+		char tmp = (char) Serial.read();	//get the next character
+
+		matlab_input += tmp;
+
+		if(tmp == '\n')
+			matlab_input_done = true;		//set the input flag
+	}
+}
---
+++ get_data_from_arduino.m
@@ -0,0 +1,58 @@
+instrreset
+delete(instrfindall)
+s = serial('COM4', 'Baudrate', 2000000);
+s.Terminator = 'LF';
+
+fopen(s);
+pause(3);
+
+data = 1:1:100;
+t = 1:1:100;
+data = zeros(size(data));
+
+while (1)
+    d = get_data(s);
+    n = str2num(d);
+    data = add_data(data, n);
+    
+    pause(0.1);
+    plot(t, data);
+    axis([0, 100, 0, 255])
+    
+end
+
+%close comunication
+fclose(s);
+delete(s);
+clear s;
+
+function data = get_data(s);
+    %ask Arduino for data
+    flushinput(s);
+    flushoutput(s);
+    fprintf(s,'send_data');
+    
+    %read data from Arduino
+    data = fscanf(s);
+end
+
+
+function data_out = add_data(data_in, new_elmnt)
+    
+    %set number of iterations
+    n = 99;
+    
+    %shift data in matrix up by 1
+    while n > 0
+        data_in(n+1) = data_in(n);
+        n = n - 1;
+    end
+
+    %add new element
+    data_in(1) = new_elmnt;
+    
+    %return new matrix
+    data_out = data_in;
+end
+
+
